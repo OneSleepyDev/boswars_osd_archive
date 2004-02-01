3d29b65f2b66126179bffde0c1e81cb690059e88
n0body <n0body@boswars.org>
2004-02-01 11:45:05 +0000

    Little fixes.

--- data.bos/scripts/elites/units.lua
+++ data.bos/scripts/elites/units.lua
@@ -269,7 +269,7 @@ DefineUnitType("unit-gen", {
 	Missile = "missile-none", Priority = 20, AnnoyComputerFactor = 45,
 	Points = 100, Supply = 125, ExplodeWhenKilled = "missile-160x128-explosion",
 	Corpse = {"build-dead-body7", 0}, Type = "land",
-	MustBuildOnTop = "unit-plate1", Building = true, BuilderOutside = true,
+	--[[MustBuildOnTop = "unit-plate1", --]] Building = true, BuilderOutside = true,
 	Sounds = {
 		"selected", "gen-selected",
 		"ready", "gen-ready",

950b2ea854a3a8527d6815e16846d6f1babdc78d
Feb <feb@boswars.org>
2004-11-05 12:19:39 +0000

    Fixed tips and removed wrong tips.

--- data.bos/scripts/tips.lua
+++ data.bos/scripts/tips.lua
@@ -29,19 +29,19 @@
 --	$Id$
 
 AddTip("You can select all of your currently visible units of the same type by holding down the CTRL key and selecting a unit or by \"double clicking\" on a unit.")
-AddTip("The more workers you have collecting resources, the faster your economy will grow.")
-AddTip("Building more than one barracks will let you train more units faster.")
-AddTip("Use your workers to repair damaged buildings.")
+AddTip("The more engineers you have collecting resources, the faster your economy will grow.")
+
+AddTip("Use your engineers to repair damaged buildings.")
 AddTip("Explore your surroundings early in the game.")
 
-AddTip("You can demolish trees and rocks.")
-AddTip("Keep all workers working. Use ALT-I to find idle workers.")
+
+AddTip("Keep all engineers working. Use ALT-I to find idle engineers.")
 AddTip("You can make units automatically cast spells by selecting a unit, holding down CTRL and clicking on the spell icon.  CTRL click again to turn off.")
 
 -- Shift tips
 AddTip("You can give an unit an order which is executed after it finishes the current work, if you hold the SHIFT key.")
 AddTip("You can give way points, if you press the SHIFT key, while you click right for the move command.")
-AddTip("You can order a worker to build one building after the other, if you hold the SHIFT key, while you place the building.")
+AddTip("You can order an engineer to build one building after the other, if you hold the SHIFT key, while you place the building.")
 AddTip("You can build many of the same building, if you hold the ALT and SHIFT keys while you place the buildings.")
 
 AddTip("Use CTRL-V or ALT-V to cycle through the viewport configuration, you can than monitor your base and lead an attack.")
